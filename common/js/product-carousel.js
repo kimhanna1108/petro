848568e46ddaa6bc2e220c9190617d139508da99
hannakim <kimhanna1108@gmail.com>
2025-05-29 15:02:42 +0900

    feat : duradrive css,js 추가

---
+++ common/js/product-carousel.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+    $('.next').on('click', function () {
+        var currentBlock = $('.hex-block.active');
+        var nextBlock = currentBlock.next();
+
+        if (nextBlock.length) {
+            currentBlock.removeClass('active');
+            nextBlock.addClass('active');
+        }
+    });
+
+    $('.prev').on('click', function () {
+        var currentBlock = $('.hex-block.active');
+        var prevBlock = currentBlock.prev();
+
+        if (prevBlock.length) {
+            currentBlock.removeClass('active');
+            prevBlock.addClass('active');
+        }
+    });
+    $('.next-gold').on('click', function () {
+        var currentBlock = $('.hex-gold-block.active');
+        var nextBlock = currentBlock.next();
+
+        if (nextBlock.length) {
+            currentBlock.removeClass('active');
+            nextBlock.addClass('active');
+        }
+    });
+
+    $('.prev-gold').on('click', function () {
+        var currentBlock = $('.hex-gold-block.active');
+        var prevBlock = currentBlock.prev();
+
+        if (prevBlock.length) {
+            currentBlock.removeClass('active');
+            prevBlock.addClass('active');
+        }
+    });
+});
